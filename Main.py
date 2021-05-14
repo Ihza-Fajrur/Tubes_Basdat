eb0f53564d7af19ac2ba86ac9732ca162feb9679
Ihza-Fajrur <ihza.119140130@student.itera.ac.id>
2021-05-14 21:34:57 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-Print("Ihza Goblok")
+import mysql.connector
+import string
+import random
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database ="penjualan_tiket_pesawat"
+)
+
+mycursor = mydb.cursor()
+
+
+print("Login")
+print("Harap inputkan data-data dibawah ini")
+temp_1 = str(input("\tUsername\t: "))
+temp_2 = str(input("\tPassword\t: "))
+
+
+  
