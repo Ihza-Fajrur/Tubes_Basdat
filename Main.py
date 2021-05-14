196f6598ad29b06a9005d82cb1e6d23b5d3b5238
Redoqx <ridho.pmswa@gmail.com>
2021-05-14 21:43:20 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,10 +11,23 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 
-print("Login")
-print("Harap inputkan data-data dibawah ini")
-temp_1 = str(input("\tUsername\t: "))
-temp_2 = str(input("\tPassword\t: "))
+def login():
+    print("Login")
+    print("Harap inputkan data-data dibawah ini")
+    Username = str(input("\tUsername\t: "))
+    Password = str(input("\tPassword\t: "))
+    if not (Username and Password):
+        print("Username dan Password tidak dapat dikosongkan")
+        return 0
+    mycursor.execute("Select password from pembeli where Username  = '" + Username + "\'")
+    result = mycursor.fetchall()
+    if not result:
+        print("Username tidak ditemukan, silakahkan sign up terlebih dahulu!")
+        return 0
+    result = (result[0])[0]
+    if result == Password:
+        print("Anda Berhasil Login!")
+        #pergi ke menu()
 
-
-  
+    else:
+        print("Username atau Password Salah!")
