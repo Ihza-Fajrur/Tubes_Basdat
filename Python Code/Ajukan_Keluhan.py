0399ba76580f84b218a9c1b7bf5806c174e46b73
Ihza-Fajrur <ihza.119140130@student.itera.ac.id>
2021-05-15 21:38:40 +0700

    Update Ajukan_Keluhan.py

--- Python Code/Ajukan_Keluhan.py
+++ Python Code/Ajukan_Keluhan.py
@@ -1,2 +1,20 @@
+import os
+import mysql.connector
+
+#Inisilasi
+clear = lambda: os.system('cls')
+pause = lambda: os.system('pause')
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  database ="penjualan_tiket_pesawat"
+)
+
+mycursor = mydb.cursor()
+
+class Current_User:
+    def __init__(self,Username):
+        self.Username = Username
+
 def Main():
     pass
